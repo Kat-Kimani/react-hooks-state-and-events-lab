5d56cdb2e02d05f3a8d49c62e9bbf7479423350b
catherine <wachukimani@gmail.com>
2023-04-21 00:28:06 +0300

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  const [addtoCart, setaddtoCart] = useState(false);
+
+  const handleCart = () => {
+    setaddtoCart(!addtoCart);
+  };
+
   return (
-    <li className="">
+    <li className={addtoCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>
+        {addtoCart ? "Add To Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
